02f86ea64601460385caf4037233e3609b378989
Moises-Developer <2120300369@soy.utj.edu.mx>
2021-07-27 23:42:09 -0500

    Realice en esta version los cambios en el login, especificamente realice solamente el Nav para despues seguir con el desarrollo del formulario

---
+++ src/Views/Login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ingreso De Sesion</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="../css/Login.css" rel="stylesheet"/>
+</head>
+<body>
+    <nav>
+        <div class="collapse" id="navbarToggleExternalContent">
+            <div class="bg-dark p-4 dPiv">
+                <img src="../../Images/login/CinePro.png" alt="No se encontro la imagen" class="img-responsive nap"/>
+                <div>
+                    <h5 class="text-white h4">CINE_PRO_MAX_MX</h5>
+                    <span class="text-muted">CineProMax es una empresa de cine dedicada a dar un mejor servicio de Peliculas</span>
+                </div>
+            </div>
+        </div>
+        <nav class="navbar navbar-dark bg-dark">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+        </nav>
+    </nav>
+    <section></section>
+
+<script type="text/javascript" src="../../Library/Own Libraries/"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> 
+<script src="../Javascript/Login.js" ></script>   
+</body>
+</html>
